8570514871fce0972af0db29146b55a903a930c2
geshah <geshah@mail.ru>
2024-08-09 20:41:08 +0600

    Add files via upload

---
+++ variables/task6.py
@@ -0,0 +1,7 @@
+quantity_home_task=12#количество дом заданий
+quantity_hours_spent=1.5#количество затраченных часов
+course_name='Python'#название курса
+print(quantity_hours_spent/quantity_home_task)
+time_one_task=(quantity_hours_spent/quantity_home_task)#время на одно задание
+print('Курс:',course_name,',всего задач:',quantity_home_task,',затраченно часов:',quantity_hours_spent,',среднее время выполнения',time_one_task,'часа.')
+
