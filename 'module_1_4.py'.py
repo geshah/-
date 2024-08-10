f30c922b530244fa4f80815407cea1bfaf763b90
geshah <geshah@mail.ru>
2024-08-10 17:40:41 +0600

    Add files via upload

---
+++ 'module_1_4.py'.py
@@ -0,0 +1,7 @@
+my_string=input('Вова это кто? ')
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
