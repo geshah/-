6a7374f20b4a7d177606e74094d3b5e0674f8688
geshah <geshah@mail.ru>
2024-08-17 09:33:00 +0600

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -5,5 +5,5 @@ if first_ == second_ == third_:
     print(3)
 elif first_ == second_ or second_ == third_ or first_ == third_:
     print(2)
-elif first_ != second_ and second_ != third_ and first_ != third_:
+else:
     print(0)
